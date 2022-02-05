a6bc3f656ec904b70f7b8bae05185f96cb2d3d4c
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-02-05 13:35:09 -0600

    set up

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
+// const users = require("./routes/api/users");
+// const tweets = require("./tweets");
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
