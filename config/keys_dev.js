db9c9189a30ed37dec4aa46fdf76989d1bfe1cd1
JJZhang <92764807+jjzhang329@users.noreply.github.com>
2022-02-05 23:58:12 -0600

    mern tweets

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dev:T5!wpJK2B6pREpS@cluster0.q4kls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    //Make sure this is your own unique string
+    secretOrKey: 'secret'
+}
